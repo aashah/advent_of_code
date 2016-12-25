1b8fe2e222a8273486672eaec744b4e1aade2608
Aakash Shah <ashah@pivotal.io>
2016-12-25 00:12:14 -0700

    day3part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -17,12 +17,23 @@ fn parse_input(file: &String) -> Vec<(u32, u32, u32)> {
 
     let file = File::open(file).expect("Unable to open file");
     let reader = BufReader::new(&file);
+
+    let mut buffer:Vec<u32> = vec![];
     for line in reader.lines() {
         let data = line.expect("Bad line data");
         let side_lengths:Vec<u32> = data.split_whitespace()
             .map(|x| x.parse::<u32>().expect("not a number"))
             .collect();
-        result.push((side_lengths[0], side_lengths[1], side_lengths[2]));
+        buffer.push(side_lengths[0]);
+        buffer.push(side_lengths[1]);
+        buffer.push(side_lengths[2]);
+
+        if buffer.len() == 9 {
+            result.push((buffer[0], buffer[3], buffer[6]));
+            result.push((buffer[1], buffer[4], buffer[7]));
+            result.push((buffer[2], buffer[5], buffer[8]));
+            buffer.clear();
+        }
     }
     result
 }
