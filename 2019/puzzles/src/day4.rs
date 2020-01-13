37a65ec048aa7283e667cdc54cc8a634cea78747
Aakash Shah <ashah@pivotal.io>
2020-01-13 09:59:31 -0800

    d4p2

--- 2019/puzzles/src/day4.rs
+++ 2019/puzzles/src/day4.rs
@@ -1,11 +1,12 @@
+use std::collections::HashMap;
+
 const NUM_A: u32 = 272091;
 const NUM_B: u32 = 815432;
 
 fn is_number_valid(num: u32) -> bool {
     let num_as_str = num.to_string();
-
     let mut last_char = '0';
-    let mut found_duplicate = false;
+    let mut char_counts: HashMap<char, u32> = HashMap::new();
 
     for c in num_as_str.chars() {
         if c < '0' || c > '9' {
@@ -15,14 +16,13 @@ fn is_number_valid(num: u32) -> bool {
             return false;
         }
 
-        if c == last_char {
-            found_duplicate = true;
-        }
+        let char_count = char_counts.entry(c).or_insert(0);
+        *char_count += 1;
 
         last_char = c;
     }
 
-    found_duplicate
+    char_counts.iter().any(|(_, v)| *v == 2)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
@@ -43,8 +43,10 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
 mod tests {
     #[test]
     fn test_number_is_valid() {
-        assert_eq!(super::is_number_valid(111111), true);
-        assert_eq!(super::is_number_valid(223450), false);
-        assert_eq!(super::is_number_valid(123789), false);
+        assert_eq!(super::is_number_valid(123456), false);
+        assert_eq!(super::is_number_valid(112233), true);
+        assert_eq!(super::is_number_valid(123444), false);
+        assert_eq!(super::is_number_valid(111122), true);
+        assert_eq!(super::is_number_valid(123333), false);
     }
 }
