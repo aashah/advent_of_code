004e90ba683bc4b95ce26685ad3e37b7b1f61ac8
Aakash Shah <ashah@pivotal.io>
2020-01-12 20:45:53 -0800

    d2p2

--- 2019/puzzles/src/day2.rs
+++ 2019/puzzles/src/day2.rs
@@ -4,25 +4,34 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let filename = "data/day2.txt";
     let input = fs::read_to_string(filename)?;
 
-    let mut program: Vec<u32> = input
+    let program: Vec<u32> = input
         .split(',')
         .map(|entry| entry.trim())
         .map(|instruction| -> u32 { instruction.parse::<u32>().expect("invalid instruction") })
         .collect();
 
-    // go into 1202 state
-    program[1] = 12;
-    program[2] = 2;
+    for i in 0..100 {
+        for j in 0..100 {
+            let mut new_program = program.clone();
+            new_program[1] = i;
+            new_program[2] = j;
+            run(&mut new_program);
 
-    run(&mut program);
+            if new_program[0] == 19690720 {
+                println!("Found the answer at ({}, {})", i, j);
+                println!("100 * noun + verb = {}", 100 * i + j);
+                return Ok(());
+            }
+        }
+    }
 
-    println!("Value at position 0: {}", program[0]);
     Ok(())
 }
 
 fn binary_op(pc: usize, program: &mut Vec<u32>, f: fn(u32, u32) -> u32) -> usize {
     let op1 = program[pc + 1] as usize;
     let op2 = program[pc + 2] as usize;
+
     let store_addr = program[pc + 3] as usize;
     program[store_addr] = f(program[op1], program[op2]);
 
