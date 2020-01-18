f8e46d56ae2cc45ad5fa4a4e5b416eac51eb9bd9
Aakash Shah <ashah@pivotal.io>
2020-01-17 16:24:04 -0800

    d5p2
    * Add 4 new op-codes, "jump-if-true", "jump-if-false", "less-than", "equals"

--- 2019/src/day5.rs
+++ 2019/src/day5.rs
@@ -10,7 +10,7 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
         .map(|instruction| -> i32 { instruction.parse::<i32>().expect("invalid instruction") })
         .collect();
 
-    let mut program = Program::new(memory, 1);
+    let mut program = Program::new(memory, 5);
     program.run();
     println!("{:?}", program.output);
 
@@ -69,6 +69,62 @@ impl Program {
                     let val = self.load(self.program[pc + 1], &instruction.modes[0]);
                     self.output.push(val);
                     pc += 2;
+                }
+                5 => {
+                    // Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the
+                    // instruction pointer to the value from the second parameter. Otherwise, it
+                    // does nothing.
+                    let val = self.load(self.program[pc + 1], &instruction.modes[0]);
+                    if val != 0 {
+                        pc = self.load(self.program[pc + 2], &instruction.modes[1]) as usize;
+                        continue;
+                    }
+
+                    pc += 3;
+                }
+                6 => {
+                    // Opcode 6 is jump-if-false: if the first parameter is zero, it sets the
+                    // instruction pointer to the value from the second parameter. Otherwise, it
+                    // does nothing.
+                    let val = self.load(self.program[pc + 1], &instruction.modes[0]);
+                    if val == 0 {
+                        pc = self.load(self.program[pc + 2], &instruction.modes[1]) as usize;
+                        continue;
+                    }
+
+                    pc += 3;
+                }
+                7 => {
+                    // Opcode 7 is less than: if the first parameter is less than the second parameter,
+                    // it stores 1 in the position given by the third parameter. Otherwise, it stores
+                    // 0.
+                    let op1 = self.load(self.program[pc + 1], &instruction.modes[0]);
+                    let op2 = self.load(self.program[pc + 2], &instruction.modes[1]);
+                    let store_addr = self.program[pc + 3] as usize;
+
+                    if op1 < op2 {
+                        self.program[store_addr] = 1;
+                    } else {
+                        self.program[store_addr] = 0;
+                    }
+
+                    pc += 4;
+                }
+                8 => {
+                    // Opcode 8 is equals: if the first parameter is equal to the second parameter,
+                    // it stores 1 in the position given by the third parameter. Otherwise, it stores
+                    // 0.
+                    let op1 = self.load(self.program[pc + 1], &instruction.modes[0]);
+                    let op2 = self.load(self.program[pc + 2], &instruction.modes[1]);
+                    let store_addr = self.program[pc + 3] as usize;
+
+                    if op1 == op2 {
+                        self.program[store_addr] = 1;
+                    } else {
+                        self.program[store_addr] = 0;
+                    }
+
+                    pc += 4;
                 }
                 99 => break,
                 _ => panic!("bad instruction"),
@@ -136,5 +192,40 @@ mod tests {
         let mut program = super::Program::new(vec![3, 1, 99], 1);
         program.run();
         assert_eq!(program.program, vec!(3, 1, 99));
+    }
+
+    #[test]
+    fn imode_simple_jump_if_true() {
+        let mut program = super::Program::new(vec![1105, 1, 4, 99, 104, 42, 99], 1);
+        program.run();
+        assert_eq!(program.output, vec!(42));
+    }
+
+    #[test]
+    fn imode_simple_jump_if_false() {
+        let mut program = super::Program::new(vec![1106, 0, 4, 99, 104, 42, 99], 1);
+        program.run();
+        assert_eq!(program.output, vec!(42));
+    }
+
+    #[test]
+    fn imode_less_than() {
+        let mut program = super::Program::new(vec![1107, 40, 41, 5, 104, -1, 99], 1);
+        program.run();
+        assert_eq!(program.output, vec!(1));
+    }
+
+    #[test]
+    fn imode_equals() {
+        let mut program = super::Program::new(vec![1108, 42, 42, 5, 104, -1, 99], 1);
+        program.run();
+        assert_eq!(program.output, vec!(1));
+    }
+
+    #[test]
+    fn imode_equals_ex1() {
+        let mut program = super::Program::new(vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], 9);
+        program.run();
+        assert_eq!(program.output, vec!(0));
     }
 }
