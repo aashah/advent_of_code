c5d57cf055af0acc07116c5040876af80460f1d2
Aakash Shah <aashah@github.com>
2020-12-18 13:53:40 -0800

    day1; part2

--- 2020/src/day1.rs
+++ 2020/src/day1.rs
@@ -10,14 +10,13 @@ fn main() {
         .lines()
         .map(|line| line.expect("failed to parse line").parse::<u32>().unwrap())
         .collect();
-    let pair: Vec<u32> = day_one_part_one(parsed_integers, 2020);
+    let pair: Vec<u32> = day_one_part_two(parsed_integers, 2020);
     println!(
-        "{} + {} = 2020; {} * {} = {}",
+        "{} + {} + {}= 2020; multiplied = {}",
         pair[0],
         pair[1],
-        pair[0],
-        pair[1],
-        pair[0] * pair[1]
+        pair[2],
+        pair[0] * pair[1] * pair[2]
     )
 }
 fn day_one_part_one(numbers: Vec<u32>, target: u32) -> Vec<u32> {
@@ -30,6 +29,18 @@ fn day_one_part_one(numbers: Vec<u32>, target: u32) -> Vec<u32> {
     }
     return vec![];
 }
+fn day_one_part_two(numbers: Vec<u32>, target: u32) -> Vec<u32> {
+    for x in &numbers {
+        for y in &numbers {
+            for z in &numbers {
+                if x + y + z == target {
+                    return vec![*x, *y, *z];
+                }
+            }
+        }
+    }
+    return vec![];
+}
 
 mod tests {
     #[test]
@@ -37,6 +48,14 @@ mod tests {
         assert_eq!(
             super::day_one_part_one(vec![1721, 979, 366, 299, 675, 1456], 2020),
             vec![1721, 299]
+        );
+    }
+
+    #[test]
+    fn day_one_part_two() {
+        assert_eq!(
+            super::day_one_part_two(vec![1721, 979, 366, 299, 675, 1456], 2020),
+            vec![979, 366, 675]
         );
     }
 }
